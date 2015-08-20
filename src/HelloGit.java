4a45865cec029a63d176ce59ab8b22858900bc23
fengweizheng <fengweizheng@meituan.com>
2015-08-20 22:53:31 +0800

    Append GPL

---
+++ src/HelloGit.java
@@ -0,0 +1,11 @@
+/**
+ * Created by fengweizheng on 15/8/20.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("welcome to git's world");
+        System.out.println("night nice");
+        // Append GPL
+    }
+}
